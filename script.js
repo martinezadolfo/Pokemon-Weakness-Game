5a34574df2e09cf386a98c13854e870247cdb8b9
Adolfo Martinez <martinezadolfo714@gmail.com>
2018-09-05 17:38:42 -0500

    more refactoring and added arrow

--- script.js
+++ script.js
@@ -40,8 +40,9 @@ function getRandomInt(min, max) {
 }
 
 function checkAnswer(id) {
-  guess = document.getElementById('option-' + id + '-name').textContent.toLowerCase();
+  let arrow =  document.getElementById("arrow");
   let score = document.getElementById('current-score').textContent.toLowerCase().split(' ');
+  guess = document.getElementById('option-' + id + '-name').textContent.toLowerCase();
   if (guess === answer) {
     document.getElementById('option-' + id).setAttribute('src', './answers/checkmark.png');
     scoreChange = Number(score[0]);
@@ -49,9 +50,15 @@ function checkAnswer(id) {
   } else {
     document.getElementById('option-' + id).setAttribute('src', './answers/X.png');
   }
+  arrow.style.visibility = "visible";
+  arrow.addEventListener('click', function () {
+    setOptions(numbers);
+    setQuestion(newChoices);
+  });
 }
 
 function setOptions(optionsArray) {
+  document.getElementById('arrow').style.visibility = 'hidden';
   Shuffle(optionsArray).map((x, index) => { // runs through shuffled array of numbers
     let option = document.getElementById('option-' + x);
     let optionName = document.getElementById('option-' + x + '-name');
@@ -63,25 +70,24 @@ function setOptions(optionsArray) {
 
 
 function setBoard() {
-  let answer, hint;
-  let numbers = ["zero", "one", "two", "three", "four"]; 
-  let pokemonNumbers = [0, 1, 2, 3, 4];
   numbers.forEach(element => document.getElementById('option-' + element).addEventListener('click', () => {checkAnswer(element);})); // adding listeners to each option
   setOptions(numbers);
-  setQuestion(pokemonNumbers);   
-  console.log(pokemonNumbers);                                                                                                                                                                                                   
+  setQuestion(newChoices);                                                                                                                                                                                                     
 }
 
 function setQuestion(options) {
-  let shuffledNumbers = Shuffle(options);
   let pokemonPicture = document.getElementById('pokemon-picture');
   let pokemonName = document.getElementById('pokemon-name');
-  pokemonPicture.setAttribute("src", "./pokemons/" + pokemons[shuffledNumbers[0]].name + ".png"); //sets pokemon picture
-  pokemonName.innerHTML = pokemons[shuffledNumbers[0]].name.charAt(0).toUpperCase() + pokemons[shuffledNumbers[0]].name.slice(1); // sets pokemon name
-  answer = pokemons[shuffledNumbers[0]].weakness;
-  hint = pokemons[shuffledNumbers[0]].type;
+  pokemonPicture.setAttribute("src", "./pokemons/" + pokemons[options[0]].name + ".png"); //sets pokemon picture
+  pokemonName.innerHTML = pokemons[options[0]].name.charAt(0).toUpperCase() + pokemons[options[0]].name.slice(1); // sets pokemon name
+  answer = pokemons[options[0]].weakness;
+  hint = pokemons[options[0]].type;
   document.getElementById('hint-button').addEventListener('click', function() {document.getElementById('hint-type').setAttribute('src', './symbols/' + hint + '.png');}); //add listener to hint 
-  delete shuffledNumbers[0];
+  options.shift();
 }
 
+const pokemonChoices = [0, 1, 2, 3, 4];
+const numbers = ["zero", "one", "two", "three", "four"]; 
+let newChoices = Shuffle(pokemonChoices.slice());
 setBoard();
+
